95e4238bf9def2c555414ae2fcc5485142e2f79d
Cob007 <godwill2beracah@outlook.com>
2024-04-17 12:06:46 -0400

    Commemt on rewards page

--- src/component/Rewards/Rewards.jsx
+++ src/component/Rewards/Rewards.jsx
@@ -1,9 +1,11 @@
 import RewardCard from "../RewardCard/RewardCard";
-import RewardLogo from "../../assets/svg/reward.svg";
+import RewardLogo from "../../assets/svg/wins.svg";
 import "./Rewards.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL, STAGING_PATH } from "../../constant/Constant";
+import Empty from "../Empty/Empty";
+
 const Rewards = () => {
   const [data, setData] = useState([]);
 
@@ -26,6 +28,15 @@ const Rewards = () => {
     getRewardFromRemote();
   }, []);
 
+
+  const NoRewards = () => {
+    return(
+    <div className="noreward">
+    <p className="noreward__p" >No Rewards Yet</p>
+    </div>
+    )
+  }
+
   return (
     <main className="reward">
       <section className="reward__header">
@@ -34,9 +45,15 @@ const Rewards = () => {
       <div className="reward__divider" />
       <div className="reward__body">
         <section className="reward__list">
-          {data.map((element, index) => (
-            <RewardCard key={index} data={element} />
-          ))}
+          {
+            data.length === 0 ? 
+              <NoRewards />
+            :
+              data.map((element, index) => (
+              <RewardCard key={index} data={element} />
+            ))
+          }
+          
         </section>
         <section className="reward__preview">
           <img className="reward__logo"  src={RewardLogo} alt="Reward Logo" />
